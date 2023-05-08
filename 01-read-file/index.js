c2171ef74936a65625090bf93ab2f2cf6d85eddd
Mikhail <potaomg@gmail.com>
2023-05-08 23:29:43 +0300

    fix: fix code node 01-read-file

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 let pathFile = path.join(__dirname, 'text.txt');
 
-const readStream = fs.createReadStream(pathFile, 'utf-8');
+const readStream = fs.ReadStream(pathFile, 'utf-8');
 
 let data = '';
 readStream.on('data', chunk => data += chunk);
